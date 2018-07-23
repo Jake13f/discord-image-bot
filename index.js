d3ff682593e1added99cf714611eb5d1ab4fdd35
Jacob Feliciani <jake13f@hotmail.com>
2018-07-23 11:49:39 -0400

    Init commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let _proto = require('./lib/prototypes/_load');
+const auth = require('./apps.auth');
+
+const Discord = require('discord.js');
+const client  = new Discord.Client();
+
+const GoogleImages = require('google-images');
+
+// Auth --------------
+const google = new GoogleImages(auth.google.cse, auth.google.api);
+client.login(auth.discord.token);
+// End Auth ----------
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+  
+client.on('message', msg => {
+    if (msg.isMentioned(client.user.id) === false)
+        return;
+
+    const regex   = /<@\d+>.*find (.*)/gm;
+    const command = regex.exec(msg.content);
+    if (command === null) return;
+
+    let term = command[1];
+    console.log(`${msg.author.tag} searched for: ${term}`);
+
+    google.search(term).then(images => {
+        let image = images.random().url;
+
+        msg.reply(`heres your image for (${term}): ${image}`);
+    }).catch(err => {
+        console.log(err);
+    });
+});
+  
